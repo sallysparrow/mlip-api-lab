7152a5543ccd0eec6a6b17e6b321dcb929a002ed
sallysparrow <chrome_cat@outlook.com>
2025-08-29 16:22:01 -0400

    Call Gemini API

--- analyze.py
+++ analyze.py
@@ -4,16 +4,16 @@ import io
 import os
 
 gemini_api_key = 'AIzaSyB8ngAGTlAd1UNmLb_Ln4K_wN901rfRDAk'
-client = genai.Client(api_key=gemini_api_key)
+gemini_client = genai.Client(api_key=gemini_api_key)
 
 def get_llm_response(image_data: bytes) -> str:
     image = Image.open(io.BytesIO(image_data))
     # implement the call to the Gemini API here
     # docs: https://ai.google.dev/gemini-api/docs/text-generation
 
-    response = client.models.generate_content(
+    response = gemini_client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=[image, "Tell me about this instrument"]
+        contents=[image, "What is in this image?"],
     )
 
     return response.text
